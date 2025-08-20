7f006ba77af1e2493596e47d2cb8ae309b0be4c8
Jazz Ooi <38699175+jazzooi21@users.noreply.github.com>
2025-08-20 16:34:38 +0800

    Update app.py

--- quiverzlive/app.py
+++ quiverzlive/app.py
@@ -1,7 +1,7 @@
 import sys, os
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication, QProxyStyle, QStyle
-from window_main import MainWindow
+from .window_main import MainWindow
 
 
 # The following hack is needed on windows in order to show the icon in the taskbar
@@ -47,6 +47,7 @@ def main() -> None:
 if __name__ == "__main__":   # `python -m quiverzlive.app`
 
     main()
+
 
 
 
